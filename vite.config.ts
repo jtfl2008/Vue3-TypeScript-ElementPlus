a636420d9764d524322abc8affd7b8e557becefe
dongjianglong <jianglong.dong@sihua.tech>
2024-06-11 10:03:05 +0800

    feat✨: Vue Macros

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import VueMacros from 'unplugin-vue-macros/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
+    VueMacros({
+      plugins: {
+        vue: vue(),
+        vueJsx: vueJsx()
+      }
+    }),
+    VueDevTools()
   ],
   resolve: {
     alias: {
